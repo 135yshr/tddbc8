1e4cd94fa7428aec7d07f6bc2b5cab6ca6648ff6
135yshr <isago@praisefirst.jp>
2018-10-20 14:04:13 +0900

    虚部に0渡したときにエラーを返すことを確認するテストケースを追加した

--- purely_ imaginary_number.go
+++ purely_ imaginary_number.go
@@ -1,9 +1,11 @@
 package tddbc8
 
+import "errors"
+
 type PurelyImaginaryNumber struct {
 	imaginaryPart int
 }
 
-func NewPurelyImaginaryNumber(i int) *PurelyImaginaryNumber {
-	return &PurelyImaginaryNumber{i}
+func NewPurelyImaginaryNumber(i int) (*PurelyImaginaryNumber, error) {
+	return &PurelyImaginaryNumber{i}, errors.New("dummy")
 }
--- purely_ imaginary_number_test.go
+++ purely_ imaginary_number_test.go
@@ -3,15 +3,22 @@ package tddbc8
 import "testing"
 
 func Test虚部に３を渡したときに虚部３を持つ純虚数が生成できること(t *testing.T) {
-	sut := NewPurelyImaginaryNumber(3)
+	sut, _ := NewPurelyImaginaryNumber(3)
 	if sut.imaginaryPart != 3 {
 		t.Fatalf("虚部に３を渡したときに虚部３を持つ純虚数が生成できない")
 	}
 }
 
 func Test虚部に10を渡したときに虚部10を持つ純虚数が生成できること(t *testing.T) {
-	sut := NewPurelyImaginaryNumber(10)
+	sut, _ := NewPurelyImaginaryNumber(10)
 	if sut.imaginaryPart != 10 {
 		t.Fatalf("虚部に10を渡したときに虚部10を持つ純虚数が生成できない")
+	}
+}
+
+func Test虚部に0渡したときにエラーを返すこと(t *testing.T) {
+	_, err := NewPurelyImaginaryNumber(0)
+	if err == nil {
+		t.Fatalf("エラーが返ってこない")
 	}
 }
