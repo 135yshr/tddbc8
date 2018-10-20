3e1eced832d7c4628bfe1355c6ad86e6ff029b51
135yshr <isago@praisefirst.jp>
2018-10-20 14:34:08 +0900

    テストコードが重複したのでリファクタリングを行った

--- purely_ imaginary_number_test.go
+++ purely_ imaginary_number_test.go
@@ -1,35 +1,52 @@
 package tddbc8
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
-func Test虚部に３を渡したときに虚部３を持つ純虚数が生成できること(t *testing.T) {
-	sut, err := NewPurelyImaginaryNumber(3)
-	if err != nil {
-		t.Fatalf("エラーが返ってきている err = %v", err)
+func Test純虚数が生成されることを確認するテスト(t *testing.T) {
+	type args struct {
+		i int
 	}
-	if sut.imaginaryPart != 3 {
-		t.Fatalf("虚部に３を渡したときに虚部３を持つ純虚数が生成できない")
+	tests := []struct {
+		name    string
+		args    args
+		want    *PurelyImaginaryNumber
+		wantErr bool
+	}{
+		{
+			name: "虚部に３を渡したときに虚部３を持つ純虚数が生成できること",
+			args: args{
+				i: 3,
+			},
+			want: &PurelyImaginaryNumber{3},
+		},
+		{
+			name: "虚部に10を渡したときに虚部10を持つ純虚数が生成できること",
+			args: args{
+				i: 10,
+			},
+			want: &PurelyImaginaryNumber{10},
+		},
+		{
+			name: "虚部に0渡したときにエラーを返すこと",
+			args: args{
+				i: 0,
+			},
+			wantErr: true,
+		},
 	}
-}
-
-func Test虚部に10を渡したときに虚部10を持つ純虚数が生成できること(t *testing.T) {
-	sut, err := NewPurelyImaginaryNumber(10)
-	if err != nil {
-		t.Fatalf("エラーが返ってきている err = %v", err)
-	}
-	if sut.imaginaryPart != 10 {
-		t.Fatalf("虚部に10を渡したときに虚部10を持つ純虚数が生成できない")
-	}
-}
-
-func Test虚部に0渡したときにエラーを返すこと(t *testing.T) {
-	sut, err := NewPurelyImaginaryNumber(0)
-
-	if err == nil {
-		t.Fatalf("エラーが返ってこない")
-	}
-
-	if sut != nil {
-		t.Fatalf("インスタンスが返ってきている sut = %v", sut)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := NewPurelyImaginaryNumber(tt.args.i)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("NewPurelyImaginaryNumber() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewPurelyImaginaryNumber() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
