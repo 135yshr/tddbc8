02eb2f706617aef761446f05eb12bf5f1de2b96d
135yshr <isago@praisefirst.jp>
2018-10-20 13:35:28 +0900

    テストケースの名前を変更した

--- purely_ imaginary_number_test.go
+++ purely_ imaginary_number_test.go
@@ -2,7 +2,7 @@ package tddbc8
 
 import "testing"
 
-func TestNewInstance(t *testing.T) {
+func Test虚部に３を渡したときに虚部３を持つ純虚数が生成できること(t *testing.T) {
 	sut := NewPurelyImaginaryNumber(3)
 	if sut.imaginaryPart != 3 {
 		t.Fatalf("虚部に３を渡したときに虚部３を持つ純虚数が生成できない")
